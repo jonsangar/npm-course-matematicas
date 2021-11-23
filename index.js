caec937d4d42b353d0e42f3b58b1fd9f86b72687
jsanchez <jsanchez@airzonesl.es>
2021-11-23 17:20:42 +0100

    Primera versión del módulo de matemáticas básica OK

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+	/**
+	 * Suma de dos números
+	 * @example
+	 * n1 = 1, n2 = 2 => resultado 4
+	 *
+	 * @param {number} n1 Primer operando
+	 * @param {number} n2 Segundo operando
+	 * @returns 
+	 */
+	suma: function(n1, n2) {
+		return n1 + n2;
+	},
+	/**
+	 * Resta de dos números
+	 * @example
+	 * n1 = 1, n2 = 3 => resultado -2
+	 *
+	 * @param {number} n1 Primer operando
+	 * @param {number} n2 Segundo operando
+	 * @returns 
+	 */
+	resta: function(n1, n2) {
+		return n1 - n2;
+	},
+	/**
+	 * Multiplicación de dos números
+	 * @example
+	 * n1 = 2, n2 = 2 => resultado 4
+	 *
+	 * @param {number} n1 Primer operando
+	 * @param {number} n2 Segundo operando
+	 * @returns 
+	 */
+	multiplicacion: function(n1, n2) {
+		return n1 * n2;
+	},
+	/**
+	 * División de dos números
+	 * @example
+	 * n1 = 10, n2 = 2 => resultado 5
+	 *
+	 * @param {number} n1 Primer operando
+	 * @param {number} n2 Segundo operando
+	 * @returns 
+	 */
+	division: function(n1, n2) {
+	 return n1 / n2;
+	}
+}
